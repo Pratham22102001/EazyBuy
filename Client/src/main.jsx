2c0be3504fbc5d29dce770dcc1ad1013dfe0af5d
Pratham22102001 <pratham.guptapg22@gmail.com>
2024-09-10 16:42:20 +0530

    done

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -7,10 +7,10 @@ import { AuthProvider } from "./context/auth.jsx";
 import "antd/dist/reset.css";
 import { SearchProvider } from "./context/search.jsx";
 import { CartProvider } from "./context/cart.jsx";
-// import axios from "axios";
+import axios from "axios";
 
-// axios.defaults.baseURL = "https://eazybuy-1.onrender.com/";
-// axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "https://eazybuy-1.onrender.com/";
+axios.defaults.withCredentials = true;
 
 https: ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
